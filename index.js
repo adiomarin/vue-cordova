5d15e9bb8eef44e344761ab45753c7ccfed02b3d
Adio Marin <adiomarin@gmail.com>
2018-09-03 12:44:24 +0200

    bug solved notifications...

--- index.js
+++ index.js
@@ -331,12 +331,12 @@ module.exports =
 
 	exports.install = function (Vue, options, cb) {
 	  document.addEventListener('deviceready', function () {
-	    if (typeof window.cordova === 'undefined' || typeof window.cordova.plugins === 'undefined' || window.cordova.plugins.notification.local === 'undefined') {
+	    if (typeof window.cordova === 'undefined' || typeof window.cordova.plugins === 'undefined' || window.cordova.plugins.notification === 'undefined') {
 	      return cb(false);
 	    }
 
 	    // pass through the diagnostic object
-	    Vue.cordova.localNotification = window.cordova.plugins.notification.local;
+	    Vue.cordova.notification = window.cordova.plugins.notification;
 
 	    return cb(true);
 	  }, false);
--- src/plugins/cordova-plugin-local-notification.js
+++ src/plugins/cordova-plugin-local-notification.js
@@ -1,11 +1,11 @@
 exports.install = function (Vue, options, cb) {
   document.addEventListener('deviceready', () => {
-    if (typeof window.cordova === 'undefined' || typeof window.cordova.plugins === 'undefined' || window.cordova.plugins.notification.local === 'undefined') {
+    if (typeof window.cordova === 'undefined' || typeof window.cordova.plugins === 'undefined' || window.cordova.plugins.notification === 'undefined') {
       return cb(false)
     }
 
     // pass through the diagnostic object
-    Vue.cordova.localNotification = window.cordova.plugins.notification.local
+    Vue.cordova.notification = window.cordova.plugins.notification
 
     return cb(true)
 
